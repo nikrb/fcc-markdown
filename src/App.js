1050894f8d0b456e7eccfd64ece6fb8b9efaca81
nikrb <nik_rb@yahoo.com>
2017-05-15 22:00:22 +0100

    code basic text entry and markdown preview

--- src/App.js
+++ src/App.js
@@ -1,21 +1,46 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import marked from 'marked';
 
-class App extends Component {
-  render() {
+class App extends React.Component {
+  state = {
+    text: ""
+  };
+  textChange = (e) => {
+    this.setState( { text:e.target.value});
+  };
+  createMarkup = () => {
+    let txt = this.state.text;
+    if( txt === "") txt = "type some text";
+    return { __html: marked( txt)};
+  };
+  render = () => {
+    const ip_text = {
+    };
+    const marked_up = {
+      width: "400px",
+      border: "1px solid black"
+    };
+    const outer = {
+      display: "flex",
+      flexDirection: "column"
+    };
+    const preview_wrapper = {
+      display: "flex",
+      flexDirection: "row",
+      justifyContent: "space-around"
+    };
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <div style={outer}>
+        <h1>Markdown Previewer</h1>
+        <div style={preview_wrapper}>
+          <div>
+            <textarea rows="12" cols="40" style={ip_text} onChange={this.textChange} value={this.state.text} />
+          </div>
+          <div style={marked_up} dangerouslySetInnerHTML={this.createMarkup()} />
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
-  }
+  };
 }
 
 export default App;
