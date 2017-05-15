e4b165be7e316250df8f96e02c19ef338d08519b
nikrb <nik_rb@yahoo.com>
2017-05-16 00:48:55 +0100

    add basic split pane

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import marked from 'marked';
 import './App.css';
+import SplitPane from 'react-split-pane';
 
 class App extends React.Component {
   state = {
@@ -15,15 +16,6 @@ class App extends React.Component {
     return { __html: marked( txt)};
   };
   render = () => {
-    const ip_text = {
-      margin: "0 10px",
-      flex: "1 1 auto"
-    };
-    const marked_up = {
-      margin: "0 10px",
-      flex: "1 1 auto",
-      border: "1px solid black"
-    };
     const outer = {
       display: "flex",
       flexDirection: "column"
@@ -33,20 +25,16 @@ class App extends React.Component {
       flexDirection: "row",
       justifyContent: "space-around"
     };
-    const splitter_bar = {
-      width: "0px",
-      height: "400px",
-      border: "5px solid rgba( 64, 64, 255, 0.5)"
-    };
     return (
       <div style={outer}>
         <h1>Markdown Previewer</h1>
         <div style={preview_wrapper}>
-          <div>
-            <textarea style={ip_text} onChange={this.textChange} value={this.state.text} />
-          </div>
-          <div style={splitter_bar} className="splitter_bar"/>
-          <div style={marked_up} dangerouslySetInnerHTML={this.createMarkup()} />
+          <SplitPane split="vertical" minSize={50} defaultSize={400}>
+            <div>
+              <textarea onChange={this.textChange} value={this.state.text} />
+            </div>
+            <div dangerouslySetInnerHTML={this.createMarkup()} />
+          </SplitPane>
         </div>
       </div>
     );
